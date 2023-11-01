2283360f1c0937d3823671e092489bc8c7591e4e
Joab T. Alencar <joabtorres1508@gmail.com>
2023-11-01 13:18:32 -0300

    ...

--- layout/login.php
+++ layout/login.php
@@ -9,11 +9,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome/css/regular.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome/css/solid.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome/css/brands.min.css">
+    <!--fontawesome-free-->
+    <link rel="stylesheet" href="assets/css/fontawesome/fontawesome.min.css">
+    <!--fontawesome-free regular-->
+    <link rel="stylesheet" href="assets/css/fontawesome/regular.min.css">
+    <!--fontawesome-free brands-->
+    <link rel="stylesheet" href="assets/css/fontawesome/brands.min.css">
+    <!--fontawesome-free solid-->
+    <link rel="stylesheet" href="assets/css/fontawesome/solid.min.css">
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
 
@@ -30,26 +33,22 @@
                 <div>
                     <section class="offset-lg-1 offset-md-2 col-lg-10 me-auto">
                         <p class="text-white"><strong>Login</strong></p>
-                        <form action="index.html">
+                        <form action="index.php">
                             <div class="input-group mb-3">
-                                <span class="input-group-text bg-primary text-white border-primary p-3 pe-4 ps-4"
-                                    id="basic-addon1"><i class="fa-solid fa-user"></i></span>
-                                <input type="email" class="form-control border-primary" placeholder="Informe o e-mail"
-                                    aria-label="Informe o e-mail"
-                                    aria-describedby="basic-addon1">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text bg-primary text-white border-primary pe-4 ps-4"><i class="fa-solid fa-user"></i></div>
+                                </div>
+                                <input type="email" class="form-control border-primary" placeholder="Informe o e-mail" aria-label="Informe o e-mail" aria-describedby="basic-addon1">
                             </div>
                             <div class="input-group">
-                                <span class="input-group-text bg-primary text-white border-primary p-3 pe-4 ps-4"
-                                    id="basic-addon1"><i class="fa-solid fa-lock"></i></span>
-                                <input type="password" class="form-control" placeholder="Informe a senha"
-                                    aria-label="Informe a senha"
-                                    aria-describedby="basic-addon1">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text bg-primary text-white border-primary pe-4 ps-4"><i class="fa-solid fa-lock"></i></div>
+                                </div>
+                                <input type="password" class="form-control" placeholder="Informe a senha" aria-label="Informe a senha" aria-describedby="basic-addon1">
                             </div>
                             <div>
-                                <div class="m-0 text-end text-secondary mt-1 mb-1">Esqueceu a senha? <a href="#"
-                                        class="text-primary text-decoration-none">Clique aqui</a></div>
-                                <button class="btn btn-primary pe-4 ps-4"><i
-                                        class="fa-solid fa-right-to-bracket"></i>
+                                <div class="m-0 text-end text-secondary mt-1 mb-1">Esqueceu a senha? <a href="#" class="text-primary text-decoration-none">Clique aqui</a></div>
+                                <button class="btn btn-primary pe-4 ps-4"><i class="fa-solid fa-right-to-bracket"></i>
                                     Entrar</button>
                             </div>
                         </form>
@@ -59,8 +58,7 @@
                     </section>
                 </div>
                 <footer class="text-center text-secondary">
-                    <i class="fa-solid fa-copyright"></i> 2023 - Desenvolvido com <i
-                        class="fa-solid fa-heart text-danger"></i> por <a href="#" class="text-decoration-none">Joab
+                    <i class="fa-solid fa-copyright"></i> 2023 - Desenvolvido com <i class="fa-solid fa-heart text-danger"></i> por <a href="#" class="text-decoration-none">Joab
                         Torres</a>
                 </footer>
             </div>
